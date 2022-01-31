bcbfb496eb426748ed61e5011cee811fc13e9504
Isra08dc <90532141+Isra08dc@users.noreply.github.com>
2022-01-31 17:43:26 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -29,18 +29,24 @@ function draw() {
 
   if(keyDown(LEFT_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
+    updateHeight(-10,0);
     //escribe el código para mover el globo aerostático en dirección hacia la izquierda
   }
   else if(keyDown(RIGHT_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
+    updateHeight(10,0);
     //escribe el código para mover el globo aerostático en dirección hacia la derecha
   }
   else if(keyDown(UP_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
+    updateHeight(0,-10);
+    balloon.scale=balloon.scale -0.001;
     //escribe el código para mover el globo aerostático en dirección ascendente
   }
   else if(keyDown(DOWN_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
+    updateHeight(0,10);
+    balloon.scale=balloon.scale +0.001;
     //escribe el código para mover el globo aerostático en dirección descendente
   }
 
@@ -49,4 +55,20 @@ function draw() {
   stroke("white");
   textSize(25);
   text("**¡Utiliza las teclas de flecha para mover el globo aerostático!",40,40);
+}
+
+function updateHeight(x,y){
+  database.ref('balloon/height').set({
+    'x': height.x+x,
+    'y': height.y+y
+  })
+}
+function readHeight(data){
+  height = data.val();
+  console.log(height.x);
+  balloon.x = height.x;
+  balloon.y = height.y;
+}
+function showError(){
+  console.log("Error in writting to the data base");
 }
